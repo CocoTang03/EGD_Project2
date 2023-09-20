b56248725eb6c60633a47fdd4684a29a28e9e92f
CocoTang03 <qinwentang@outlook.com>
2023-09-19 20:27:47 -0400

    A general Start Page

--- EGD_Project2/Assets/Scripts/UIManager.cs
+++ EGD_Project2/Assets/Scripts/UIManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     private Animator enterScene;
     private Animator intoScene;
+
+    private bool creditOn = false;
+    private bool rankingOn = false;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -23,7 +28,18 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(creditOn)
+            {
+                CreditsOnClick();
+            }
+            if(rankingOn)
+            {
+                RankOnClick();
+            }
+        }
+
     }
 
     //
@@ -32,5 +48,20 @@ public class UIManager : MonoBehaviour
         // Reverse play the animation 
 
         SceneManager.LoadScene("XRace");
+    }
+
+    public void CreditsOnClick() 
+    {
+        GameObject credits = GameObject.Find("Credits");
+        credits.GetComponent<Image>().enabled = !credits.GetComponent<Image>().enabled;
+        credits.GetComponentInChildren<TextMeshProUGUI>().enabled = !credits.GetComponentInChildren<TextMeshProUGUI>().enabled;
+        creditOn = !creditOn;
+    }
+    public void RankOnClick()
+    {
+        GameObject credits = GameObject.Find("Ranking");
+        credits.GetComponent<Image>().enabled = !credits.GetComponent<Image>().enabled;
+        credits.GetComponentInChildren<TextMeshProUGUI>().enabled = !credits.GetComponentInChildren<TextMeshProUGUI>().enabled;
+        rankingOn = !rankingOn;
     }
 }
